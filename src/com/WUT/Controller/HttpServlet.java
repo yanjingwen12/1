2891eb94a075500d753c3c6049692b3083293939
qwe <qwe@LAPTOP-1FHFRC7O>
2019-12-28 20:41:35 +0800

    简单的建了一些东西，先用着

---
+++ src/com/WUT/Controller/HttpServlet.java
@@ -0,0 +1,7 @@
+package com.WUT.Controller;
+
+public class HttpServlet {
+public HttpServlet() {
+	// TODO Auto-generated constructor stub
+}
+}
---
+++ src/com/WUT/model/New.java
@@ -0,0 +1,5 @@
+package com.WUT.model;
+
+public class New {
+
+}
